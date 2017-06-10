81a0c0646d75e8496bda74fc82be5789fd0e678b
Adler Coelho <adler.coelho.santos@icloud.com>
2017-06-10 14:56:31 -0300

    Implementando o EJS.

--- aula 01/casa-do-codigo/app.js
+++ aula 01/casa-do-codigo/app.js
@@ -1,12 +1,13 @@
-const express = require("express")();
+const express = require("express")()
+  .set("view engine", "ejs");
 const port = 3000;
 
 express.get("/produtos", (req, res) => {
 
-  res.send("<html><body><h1>hello world!</h1></body></html>");
+  res.render("produtos/lista");
 
 });
 
 express.listen(port, () => {
   console.log("servidor em pe");
-})
+});
